4346a990242e56fe1b2751b634c870181b87e4eb
Ubuntu <ubuntu@ip-172-31-1-85.ap-south-1.compute.internal>
2022-07-31 11:39:03 +0000

    added

--- aws/ec2-module/main.tf
+++ aws/ec2-module/main.tf
@@ -5,28 +5,8 @@ terraform {
   }
 }
 
-module "aws_ebs_volume"{
-  source = "../modules/aws_modules/"
-  //availability_zone = "ap-south-1a"
-  //aws_ebs_volume_size = 8
-}
-
 module "aws_instance" {
   source = "../modules/aws_modules/"
-  //aws_instance_name = "mytestservernew"
-}
-
-module "aws_key_pair" {
-  source = "../modules/aws_modules/"
-  //aws_key_name = "My-Testserver-keya"
-}
-module "tls_private_key" {
-  source = "../modules/aws_modules/" 
-}
-module "aws_security_group" {
-  source = "../modules/aws_modules/"
-  //aws_security_group_name = "my-golbal-security-group"
-  //aws_security_group_to_port = 22
 }
 
 
